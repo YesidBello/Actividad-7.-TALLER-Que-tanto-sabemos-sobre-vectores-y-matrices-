b4ff23d93aecbf8ee1f03bb5d00342a2afe0b53c
Yesid Bello <wibello78@unisalle.edu.co>
2026-05-22 18:01:17 -0500

    Aplicando mejoras

--- Problema_1.py
+++ Problema_1.py
@@ -1,22 +1,30 @@
+# Importamos el módulo que contiene el algoritmo matemático para generar
+# números aleatoriamente
 import random
 
+# Imprimimos un aviso de lo que el algoritmo hará
+print("\nHola! Tu me dirás un número (N) y yo haré una lista de (N) números al"
+      " azar para luego ordenarlos de mayor a menor")
 
+# Ciclo infinito que se romperá hasta que el usuario ingrese un dato válido
 while True:
     try:
         n_elementos = int(input(
-            "\nIngresa la cantidad positiva de elementos que desees: ")
+            "Ingresa la cantidad positiva de elementos que desees: ")
                           )
         if n_elementos > 0:
-            print("¡Número válido!\n")
+            print("----------¡Número válido!----------\n")
             break
         else:
             print("¡El número debe ser positivo!\n")  
     except ValueError:
         print("¡Ese no es un número!")
-        print("Ingresa solamente números\n")              
-                
+        print("Ingresa solamente dígitos\n")
+        
+# Creamos una lista donde se almacenarán los numeros generados aleatoriamente
 lista_numeros = []
 
+# Ciclo que genera los números
 for _ in range(n_elementos):
 
     numeros_generados = random.randint(1, 1000)
@@ -24,8 +32,10 @@ for _ in range(n_elementos):
 
 print("Lista de números al azar: ", lista_numeros)
 
+# Iniciamos un booleano
 hubo_intercambio = True
 
+# Creamos un ciclo que finaliza cuando no se haga un intercambio más
 while hubo_intercambio:
     hubo_intercambio = False
 
@@ -37,10 +47,8 @@ while hubo_intercambio:
                 lista_numeros[i],
             )
             hubo_intercambio = True
-print("--------------------------------------------------------------------"
-      "---------------------------------------------------------------------")
-print("--------------------------------------------------------------------"
-      "-------------------------------------------------------------------")
+print("\n-----------------------------------------------------------------\n")
+
 
 print("Lista de números ordenados de mayor a menor: ", lista_numeros)
 print("\n")
---
+++ Problema_2.py
@@ -0,0 +1,6 @@
+import random
+
+def fill_matrix(n):
+    
+
+
