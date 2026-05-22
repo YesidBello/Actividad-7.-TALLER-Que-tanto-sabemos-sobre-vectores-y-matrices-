3f14f163f9bc7803063c441e1845c182b1c7d0e1
Yesid Bello <wibello78@unisalle.edu.co>
2026-05-21 19:47:05 -0500

    Solucion al Problema 1: Burbuja y validaciones

---
+++ Problema_1.py
@@ -0,0 +1,47 @@
+import random
+
+
+while True:
+    try:
+        n_elementos = int(input(
+    "Ingresa la cantidad positiva de elementos que desees: "))
+        if n_elementos > 0:
+            print("¡Número válido!")
+            break
+        else:
+            print("¡El número debe ser positivo!")
+            
+    except ValueError:
+        print("¡Ese no es un número!")
+        print("Ingresa solamente números")              
+                
+lista_numeros = []
+
+for _ in range(n_elementos):
+
+    numeros_generados = random.randint(1, 1000)
+    lista_numeros.append(numeros_generados)
+
+print("Lista de números al azar: ", lista_numeros)
+
+hubo_intercambio = True
+
+while hubo_intercambio:
+    hubo_intercambio = False
+
+    for i in range(n_elementos - 1):
+
+        if lista_numeros[i] < lista_numeros[i + 1]:
+            lista_numeros[i], lista_numeros[i + 1] = (
+                lista_numeros[i + 1],
+                lista_numeros[i],
+            )
+            hubo_intercambio = True
+print("--------------------------------------------------------------------"
+      "---------------------------------------------------------------------")
+print("--------------------------------------------------------------------"
+      "-------------------------------------------------------------------")
+
+print("Lista de números ordenados de mayor a menor: ", lista_numeros)
+\
+
