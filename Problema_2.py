c3353a36b95251381bbc93728ae131bd0dc86d46
Yesid Bello <wibello78@unisalle.edu.co>
2026-05-22 20:39:22 -0500

    Creacion de matriz NxN

--- Problema_2.py
+++ Problema_2.py
@@ -1,6 +1,38 @@
+# Importamos el módulo que contiene el algoritmo matemático para generar
+# números aleatoriamente
 import random
+       
+# Imprimimos un aviso de lo que el algoritmo hará
+print("\nHola! Calcularemos el determinante de una matríz NxN"
+      " tu me dirás el número (N) que prefieras")
 
-def fill_matrix(n):
-    
+# Ciclo infinito que se romperá hasta que el usuario ingrese un dato válido
+while True:
+    try:
+        numero = int(input(
+            "Ingresa la cantidad positiva de elementos que desees: ")
+                          )
+        if numero > 0:
+            print("----------¡Número válido!----------\n")
+            break
+        else:
+            print("¡El número debe ser positivo!\n")  
+    except ValueError:
+        print("¡Ese no es un número!")
+        print("Ingresa solamente dígitos\n")
 
+# Creamos el método para llenar la matríz
+def fill_matrix(numero):
+    matriz = []
 
+# Ciclo para crear las filas
+    for _ in range(numero):  
+        fila = []
+        
+# Ciclo para crear las columnas
+        for _ in range(numero):
+            numeros_aleatorios = random.randint(1, 20) 
+            fila.append(numeros_aleatorios)
+        matriz.append(fila)
+    return matriz
+print(fill_matrix(numero))
